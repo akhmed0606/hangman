141d144dfe9262230079282fbf7636c4910ef340
akhmed0606 <analgiev57gmail.com>
2022-01-30 18:29:58 -0500

    created file to gen-te letters and added protypes

---
+++ src/components/GameWordLetter.js
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+
+const GameWordLetter = ({ char, index }) => {
+  return (
+    <span className="wordBox" key={index}>
+      {char}
+    </span>
+  );
+};
+
+GameWordLetter.propTypes = {
+  char: PropTypes.string,
+  index: PropTypes.number,
+};
+
+export default GameWordLetter;
