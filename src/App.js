abbf15a1a8a5880f3c16bfe6189b9f472f19e510
akhmed0606 <analgiev57gmail.com>
2022-01-30 15:11:30 -0500

    imported Game.js to the main compnt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
+import Game from './components/Game'
 
 function App() {
   return (
-    <div>
+    <main className='wrapper'>
       <Header />
       <Home />
+      <Game />
       <Footer />
-    </div>
+    </main>
   );
 }
 
