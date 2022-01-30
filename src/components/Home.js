5bc8af66505495acda5495fc5e883faea9330c16
akhmed0606 <analgiev57gmail.com>
2022-01-30 13:52:24 -0500

    created home component and added content

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+
+const Home = () => {
+  return (
+    <div className="gameDescription">
+      <h2>A Word Guessing Game</h2>
+      <p>
+        Try to guess the letters in an unknown word. A hint is available for a price, and up to 6 wrong guess are
+        allowed. To begin, press Start Game below!
+      </p>
+       
+       <h6> Start Game</h6>
+        <h6>Liderboard</h6>
+     
+      
+    </div>
+  );
+};
+
+export default Home;
