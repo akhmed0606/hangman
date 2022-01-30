8f2980931c47eeab1e5903bf98b2de0c7b4c7d0d
akhmed0606 <analgiev57gmail.com>
2022-01-30 18:22:04 -0500

    created state vars to start game and fetch data

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,18 +1,27 @@
-import { useState } from 'react';
+import { useState, useCallback } from "react";
+import KeyboardLetter from "./KeyBoardLetter";
 
-const Game = ({ gameWordArray, setIsGameStarted }) => {
- 
-  const [unusedLettersArray, setUnusedLettersArray] = useState('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(""));
+const Game = () => {
+  const [unusedLettersArray, setUnusedLettersArray] = useState([]);
   const [usedLettersArray, setUsedLettersArray] = useState([]);
-  const [emptyWordArray, setEmptyWordArray] = useState(gameWordArray.map((char) => (char === ' ' ? ' ' : '_')));
-  const [turnsLeft, setTurnsLeft] = useState(6);
-
-  const checkLetter = (e) => {
-    const currentLetter = e.type === 'keydown' ? e.key.toUpperCase() : e.target.textContent;
+  const [emptyWordArray, setEmptyWordArray] = useState([]);
+  const [turnsLeft, setTurnsLeft] = useState(0);
+  const [isWinner, setIsWinner] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [score, setScore] = useState(0);
+  const [showHint, setShowHint] = useState(false);
+  const [gameWordArray, setGameWordArray] = useState([]);
+  const [definition, setDefinition] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
+ 
+  const checkLetter = useCallback((e) => {
+    const currentLetter =
+      e.type === "keydown" ? e.key.toUpperCase() : e.target.textContent;
     // remove letter from unusedLettersArray
-    setUnusedLettersArray([...unusedLettersArray].filter((letter) => letter !== currentLetter));
-    // check letter in charArray
-    console.log(gameWordArray);
+    setUnusedLettersArray(
+      [...unusedLettersArray].filter((letter) => letter !== currentLetter)
+    );
+    // Check letter in charArray and return wordArray to include found letter in place of underscores
     let letterFoundInWord = false;
     const updatedWordArray = emptyWordArray.map((char, index) => {
       if (gameWordArray[index] === currentLetter) {
@@ -22,6 +31,7 @@ const Game = ({ gameWordArray, setIsGameStarted }) => {
         return char;
       }
     });
+
     // decrememnt turnsLeft if letter was incorrect
     const newTurnsLeft = turnsLeft - 1;
     if (!letterFoundInWord) {
@@ -31,45 +41,47 @@ const Game = ({ gameWordArray, setIsGameStarted }) => {
     }
 
     setEmptyWordArray(updatedWordArray);
-    if (gameWordArray.join('') === updatedWordArray.join('')) {
-      alert('You win!');
+    if (gameWordArray.join("") === updatedWordArray.join("")) {
+      alert("You win!");
       setIsGameStarted(false);
     }
     if (newTurnsLeft === 0) {
-      alert('You Lose :(');
+      alert("You Lose :(");
       setIsGameStarted(false);
     }
-  };
+  });
 
-
-
-  return(
-    <section className='gameContainer'>
-    <h2>Tries Left: {turnsLeft}</h2>
-    <div className='container'>
-      {emptyWordArray.map((char, index) => {
-        return (
-          <span className='wordBox' key={index}>
-            {char}
-          </span>
-        );
-      })}
-    </div>
-    <div className='usedLettersContainer'>
-      {usedLettersArray.map((letter) => {
-        return <span className='wordBox'>{letter}</span>;
-      })}
-    </div>
-    <div className='unusedLettersContainer'>
-      {unusedLettersArray.map((letter) => {
-        return (
-          <button onClick={checkLetter} key={letter}>
-            {letter}
-          </button>
-        );
-      })}
-    </div>
-  </section>
-  )
-}
+  return (
+    <section className="gameContainer">
+      <h2>Tries Left: {turnsLeft}</h2>
+      <div className="container">
+        {emptyWordArray.map((char, index) => {
+          return (
+            <span className="wordBox" key={index}>
+              {char}
+            </span>
+          );
+        })}
+      </div>
+      <div className="unusedLettersContainer">
+        {unusedLettersArray.map((letter) => (
+          <KeyboardLetter
+            letter={letter}
+            checkLetter={checkLetter}
+            key={letter}
+          />
+        ))}
+      </div>
+      <div className="unusedLettersContainer">
+        {unusedLettersArray.map((letter) => {
+          return (
+            <button onClick={checkLetter} key={letter}>
+              {letter}
+            </button>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 export default Game;
