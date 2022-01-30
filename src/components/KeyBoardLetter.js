3ee1cd4cd33961824cb0857105f22a353355cd46
akhmed0606 <analgiev57gmail.com>
2022-01-30 17:26:28 -0500

    made new component and destructed props from game

---
+++ src/components/KeyBoardLetter.js
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+
+const KeyboardLetter = ({ checkLetter, letter }) => {
+  return (
+    <button className="keyboardLetter" onClick={checkLetter} key={letter}>
+      {letter}
+    </button>
+  );
+};
+
+KeyboardLetter.propTypes = {
+  checkLetter: PropTypes.func,
+  letter: PropTypes.string,
+};
+
+export default KeyboardLetter;
