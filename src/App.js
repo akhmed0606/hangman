1b45e185758098a2d806f17f3875477be2137ab3
akhmed0606 <analgiev57gmail.com>
2022-01-31 16:49:42 -0500

    Latest version of router added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Game from "./components/Game";
 import Footer from "./components/Footer";
-import Game from './components/Game'
 
 function App() {
   return (
-    <main className='wrapper'>
-      <Header />
-      <Home />
-      <Game />
-      <Footer />
+    <>
+    <Header />
+    <main className="wrapper">
+    <Routes>
+      <Route  path="/" element={<Home />}> </Route>
+      <Route path="/game" element={<Game />}></Route>
+    </Routes>
     </main>
-  );
+    <Footer />
+    </>
+   
+
+);
 }
 
 export default App;
