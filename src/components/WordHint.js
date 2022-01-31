285d74806f718e5c3c644b0751fd8fd9d7ba3ba1
akhmed0606 <analgiev57gmail.com>
2022-01-31 10:18:04 -0500

    Added protypes and classes for wordhint component

--- src/components/WordHint.js
+++ src/components/WordHint.js
@@ -1,20 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const WordHint = ({ definition, showHint, setShowHint }) => {
   return (
-    <div>
+    <div className="hintBox">
       {!showHint ? (
         <button onClick={() => setShowHint(true)}>Show Hint (-200 pts)</button>
       ) : (
         <p>
-          <span>Hint:</span>
-        <p> {definition} </p> 
+          <span className="bold">Hint:</span> {definition}
         </p>
       )}
     </div>
   );
 };
 
-
+WordHint.propTypes = {
+  definition: PropTypes.string,
+  showHint: PropTypes.bool,
+  setShowHint: PropTypes.func,
+};
 
 export default WordHint;
