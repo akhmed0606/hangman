17bfe2524a0f6acdab0c921a6ced3dc6e2fca790
akhmed0606 <analgiev57gmail.com>
2022-02-01 16:07:28 -0500

    adjust firebase config.js

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,11 +1,14 @@
-import firebase from 'firebase/compat/app';
+import firebase from "firebase/app";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: `${process.env.REACT_APP_FIREBASE_ID}.firebaseapp.com`,
-  projectId: process.env.REACT_APP_FIREBASE_ID,
-  dataBaseURL: `https://${process.env.REACT_APP_FIREBASE_ID}-default-rtdb.firebaseio.com`,
+  apiKey: "AIzaSyDxfO-4udRA7VuT-3aVSl443OZ7ru3Z7Vg",
+  authDomain: "hangman-238a0.firebaseapp.com",
+  databaseURL: "https://hangman-238a0-default-rtdb.firebaseio.com",
+  projectId: "hangman-238a0",
+  storageBucket: "hangman-238a0.appspot.com",
+  messagingSenderId: "425693437828",
+  appId: "1:425693437828:web:08b946ac544c3622a687d1"
 };
 
 firebase.initializeApp(firebaseConfig);
