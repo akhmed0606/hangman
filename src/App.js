e4a0f360619f94d0bf6f9c32e14a7a02ea6139dc
akhmed0606 <analgiev57gmail.com>
2022-02-01 10:55:08 -0500

    created new file to keep truck of scores&linked

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Game from "./components/Game";
 import Footer from "./components/Footer";
+import Leaderboard from "./components/LeaderBoard";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
     <Routes>
       <Route  path="/" element={<Home />}> </Route>
       <Route path="/game" element={<Game />}></Route>
+      <Route path="/leaderboard" element={<Leaderboard />}></Route>
     </Routes>
     </main>
     <Footer />
